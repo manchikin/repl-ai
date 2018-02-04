f6b2bd292b3189233cbde49cdeaf7f79d670663c
Manchikin <murasakita24@gmail.com>
2018-02-04 21:52:59 +0900

    [ダイアログ出力] fix: 文字コードをSJISで出力するよう修正、日付と日時を分離

--- logFormatter.js
+++ logFormatter.js
@@ -1,5 +1,6 @@
 // usage node logFormatter.js [debug]
 
+const iconv = require("iconv-lite");
 const configs = require('./configs/bootload');
 const Datastore = require('nedb');
 const dateFormat = require("dateformat");
@@ -17,15 +18,17 @@ db.loadDatabase((err) => {
     console.log("Finished Loading DB. Start Sorting...");
     db.find({}).sort({ dialogue_created: 1 }).exec((err, docs) => {
         if (err) throw err;
-        let output = "日時\tdiscord id\tリクエスト\tレスポンス\n";
+        let output = "日付\t時間\tdiscord id\tリクエスト\tレスポンス\n";
         if (mode==="debug") console.log(docs);
         Object.keys(docs).forEach((key) => {
             const doc = docs[key];
-            output = output.concat(dateFormat(doc.dialogue_created, "yyyy-mm-dd HH:MM:ss") + "\t" + doc.discord_id + "\t" + doc.request_message + "\t" + doc.response_message + "\n");
+            output = output.concat(dateFormat(doc.dialogue_created, "yyyy-mm-dd") + "\t" + dateFormat(doc.dialogue_created, "HH:MM:ss") + "\t" + doc.discord_id + "\t" + doc.request_message + "\t" + doc.response_message + "\n");
         });
         if (mode==="debug") console.log(output);
+        console.log("Converting character code to SJIS.");
+        const encodedOutput = iconv.encode(output, "Shift_JIS");
         console.log("Outputting");
-        writeFile('output/dialogue_log.tsv', output);
+        writeFile('output/dialogue_log.tsv', encodedOutput);
         console.log("Finished");
     });
 });
